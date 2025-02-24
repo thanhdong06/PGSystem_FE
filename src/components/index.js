8b47eec9cb30e3229f6d1ea4b27783e8bf05eafd
TuanNguyen <huutuannguyen2604@gmail.com>
2025-02-24 14:16:02 +0700

    [Tuan]Update

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Login } from "./Login/Login";
-export { default as Home } from "./HomePage/Home";
-export { default as Header } from "../components/Header/Header";
-export { default as Footer } from "../components/footer/Footer";
