eecfefd975ba137f827ba5d072027aa489cc80d6
TuanNguyen <huutuannguyen2604@gmail.com>
2025-03-12 15:36:55 +0700

    [Tuan] Update Profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import Home from "./pages/home/Home";
